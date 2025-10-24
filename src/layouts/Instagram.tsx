543cb3f233162d95ef65465cf1dc7fcba554fedb
nestorhugo <nestorhugofc@gmail.com>
2025-10-24 02:49:07 -0300

    Criado componentes para Instagram e Stories

---
+++ src/layouts/Instagram.tsx
@@ -0,0 +1,54 @@
+// src/components/Linkedin.tsx
+import React from "react";
+import type { eventData } from "../routes/image.js";
+
+export default function InstagramImage({ event }: { event: eventData }) {
+  const containerStyle: React.CSSProperties = {
+    background: "#083A1B",
+    width: "1080px",
+    height: "1350px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "start",
+    color: "white",
+    fontFamily: "Inter, sans-serif",
+    overflowWrap: "break-word",
+    wordBreak: "break-word",
+  };
+
+  const text: React.CSSProperties = {
+    margin: 0,
+  };
+
+  const logoStyle: React.CSSProperties = {
+    width: "150px",
+  };
+
+  const containerText: React.CSSProperties = {
+    width: "500px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "10px",
+  };
+
+  return (
+    <div style={containerStyle}>
+      <div style={containerText}>
+        <img
+          src="https://devpr.org/assets/images/icone_dev_pr.svg"
+          alt="Logo"
+          style={logoStyle}
+        />
+        <h1 style={text}>{event.title}</h1>
+        <h3 style={text}>{event.subtitle}</h3>
+        <p style={text}>{event.day}</p>
+        <p style={text}>{event.address}</p>
+        <p style={text}>{event.city}</p>
+      </div>
+    </div>
+  );
+}
--- src/utils/generateImage.tsx
+++ src/utils/generateImage.tsx
@@ -4,6 +4,8 @@ import { dirname, join } from "path";
 import { readFile } from "fs/promises";
 import { fileURLToPath } from "url";
 import type { eventData } from "../routes/image.js";
+import { capitalize } from "./capitalize.js";
+import { MEDIA_SIZES } from "../constants/mediaSizes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,69 +15,27 @@ export async function generateImage(event: eventData) {
     join(__dirname, "../fonts/Inter-Regular.ttf")
   );
 
-  const containerStyle: React.CSSProperties = {
-    background: "#083A1B",
-    width: "500px",
-    height: "500px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    textAlign: "start",
-    color: "white",
-    fontFamily: "Inter, sans-serif",
-    overflowWrap: "break-word",
-    wordBreak: "break-word",
-  };
+  const media = event.media || "default";
+  const size = MEDIA_SIZES[media as keyof typeof MEDIA_SIZES];
 
-  const text: React.CSSProperties = {
-    margin: 0,
-  };
+  const module = await import(`../layouts/${capitalize(media)}.js`);
+  let Component = module.default;
 
-  const logoStyle: React.CSSProperties = {
-    width: "150px",
-  };
-
-  const containerText: React.CSSProperties = {
-    width: "500px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: "10px",
-  };
-
-  const svg = await satori(
-    <div style={containerStyle}>
-      <div style={containerText}>
-        <img
-          src="https://devpr.org/assets/images/icone_dev_pr.svg"
-          alt="Logo"
-          style={logoStyle}
-        />
-        <h1 style={text}>{event.title}</h1>
-        <h3 style={text}>{event.subtitle}</h3>
-        <p style={text}>{event.day}</p>
-        <p style={text}>{event.address}</p>
-        <p style={text}>{event.city}</p>
-      </div>
-    </div>,
-    {
-      width: 500,
-      height: 500,
-      fonts: [
-        {
-          name: "Inter",
-          data: fontData,
-          weight: 400,
-          style: "normal",
-        },
-      ],
-    }
-  );
+  const svg = await satori(<Component event={event} />, {
+    width: size.width,
+    height: size.height,
+    fonts: [
+      {
+        name: "Inter",
+        data: fontData,
+        weight: 400,
+        style: "normal",
+      },
+    ],
+  });
 
   const resvg = new Resvg(svg, {
-    fitTo: { mode: "width", value: 600 },
+    fitTo: { mode: "width", value: size.width },
   });
 
   const pngData = resvg.render();
